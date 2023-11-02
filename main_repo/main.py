60bba5f2cdf35d61b53b53eb0fc6c892db9b0b97
Pavel <pavel.gurin@skilltellect.ai>
2023-11-02 14:46:46 +0300

    add different messages for exceptions in main.py

--- main_repo/main.py
+++ main_repo/main.py
@@ -1,9 +1,6 @@
 import lib.just_library as l
 
-from typing import Union
-from fastapi import FastAPI, Response, File, UploadFile
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from fastapi import FastAPI, Response, File
 
 tags_metadata = [
     {
@@ -27,22 +24,21 @@ def find_dog(response: Response,file_bytes: bytes = File()):
     """
     found_dog = False 
     status = "ok"
-    status_code = 200
     err_msg = ""
     try:
         l_file = len(file_bytes)
         print("size_file: ", l_file)
         if l_file > max_size:
-            raise Exception
+            raise Exception("Image size > 10 MB!")
         if l_file < 1:
-            raise Exception
+            raise Exception("No data image")
 
         res = l.predict(file_bytes, len(file_bytes))
         if res == "dog":
             found_dog = True
 
     except Exception as e:
-        err_msg = "Bad image"
+        err_msg = str(e)
         status = "error"
         response.status_code = 400
     finally:
